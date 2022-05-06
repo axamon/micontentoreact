dde075ee1796df04a69d39b0fe39a6d27dcb54fe
00246506 <alberto.bregliano@telecomitalia.it>
2022-05-06 14:10:46 +0200

    crowdsource button

--- src/App.js
+++ src/App.js
@@ -10,24 +10,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="circle"></div>
+      <div class="circle2"></div>
       <div class="grid-container">
         <div class="grid-item"></div>
         <div class="grid-item"><Title titolo="MiContento"/></div>
-        <div class="grid-item"></div>  
+        <div class="grid-item"></div>
+
         <div class="grid-item"></div>
         <div class="grid-item"><Typewriter text="Unità di misura di senso."/></div>
-        <div class="grid-item"></div>  
         <div class="grid-item"></div>
-        <div class="grid-item mt-2 responsive">
-          <Tilt trackOnWindow="true" gyroscope="true">
-            <div style={{ height: '300px', backgroundColor: 'lightblue' }}>
-              <img width="100%" data-tilt src="Floral-Child-Silhouette.png" />
+
+        <div class="grid-item"></div>
+        <div class="grid-item mt-4 text-center">
+          <Tilt trackOnWindow="true" gyroscope="true" tiltReverse="true">
+          <nav>
+            <ul>
+                <li><a href="#">Contatti</a></li>
+                <li><a href="#">Sportello</a></li>
+                <li><a href="#">GPT</a></li>
+            </ul>
+          </nav>
+            <div style={{ height: '10vw', backgroundColor: 'lightblue' }}>
+              <img class="img-fluid" src="Floral-Child-Silhouette.png" />
+              <Typewriter text="Codesto solo oggi possiamo dirti: ciò che non siamo e ciò che non vogliamo." />
+
             </div>
           </Tilt>
-
          
         </div>
-        <div class="grid-item"></div>  
+        <div class="grid-item container">
+          
+        </div>  
+      </div>
+      <div className='container'>
+        <div className='crowdfounding'><button class="button-53" role="button">Crowdfounding</button></div>
       </div>
       <Footer owner="MiContento" />
     </div>
